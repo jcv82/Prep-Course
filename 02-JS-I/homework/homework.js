54961c32e7a1b1063602da13e72ee0dd4134b114
Julio Valdez <juliovaldez@yahho.com>
2022-01-22 02:03:38 -0300

    terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -182,7 +182,7 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return str + '!';
 }
 
 function obtenerSaludo(nombre) {
