b23c93277e8e58cb3e98889574f7b761d6620c70
Julio Valdez <juliovaldez@yahho.com>
2022-01-22 02:17:36 -0300

    terminado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -191,8 +191,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  var combinado = nombre + ' ' + apellido;
-  return combinado;
+  return 'Hola ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
