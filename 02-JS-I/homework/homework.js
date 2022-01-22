6b3705478911af1dffa44331d44c0fa77775dc5d
Julio Valdez <juliovaldez@yahho.com>
2022-01-22 01:13:56 -0300

    modificado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -82,8 +82,8 @@ function menosQueNoventa(num) {
     return true;
   } else {
     return false;
+  }
 }
-
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
